824f485c977ad591d5cc4b96e2e9ce36328d4099
NejcRibic <50303679+NejcRibic@users.noreply.github.com>
2024-01-01 15:58:51 +0100

    Add files via upload

--- updateAge.js
+++ updateAge.js
@@ -3,16 +3,7 @@ const currentDate = new Date();
 const age = currentDate.getFullYear() - birthDate.getFullYear();
 
 const updateAge = () => {
-  const age = currentDate.getFullYear() - birthDate.getFullYear();
-  const ageElement = document.getElementById('age-span');
-  ageElement.textContent = age;
-
-  // Check if the current year is different from the previous year
-  const previousYear = currentDate.getFullYear() - 1;
-  if (currentDate.getFullYear() !== previousYear) {
-    age++; // Increment age by one
-    ageElement.textContent = age;
-  }
+  document.getElementById('age-span').textContent = age;
 };
 
-setInterval(updateAge, 1000 * 60 * 60 * 24 * 365); // Update every 365 days (1 year)
+document.addEventListener('DOMContentLoaded', updateAge);
