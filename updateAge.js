8344d12e40fd84b0c0017bba5fc5ca9832a9920a
NejcRibic <50303679+NejcRibic@users.noreply.github.com>
2024-01-01 13:08:04 +0100

    Add files via upload

---
+++ updateAge.js
@@ -0,0 +1,9 @@
+const birthDate = new Date('05-08-2002'); // Your birthday in YYYY-MM-DD format
+const currentDate = new Date();
+const age = currentDate.getFullYear() - birthDate.getFullYear();
+
+setInterval(() => {
+  // Update the age every midnight
+  currentDate = new Date();
+  age = currentDate.getFullYear() - birthDate.getFullYear();
+}, 1000 * 60 * 60 * 24); // Update every 24 hours (1 day)
