03d48fb55cd3e8d4d65ced34b3dbde47c7035ccf
NejcRibic <50303679+NejcRibic@users.noreply.github.com>
2024-01-02 12:26:03 +0100

    Delete updateCopyrightYear.js

--- updateCopyrightYear.js
+++
@@ -1,6 +0,0 @@
-const updateCopyrightYear = () => {
-  const copyrightYear = document.getElementById('copyright-year');
-  copyrightYear.textContent = new Date().getFullYear();
-};
-
-document.addEventListener('DOMContentLoaded', updateCopyrightYear);
