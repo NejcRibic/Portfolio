50b067a1986a5e6e2f654ea1cbf7bc72e63a5d2c
NejcRibic <50303679+NejcRibic@users.noreply.github.com>
2024-01-02 12:38:15 +0100

    Delete updateAge.js

--- updateAge.js
+++
@@ -1,9 +0,0 @@
-const birthDate = new Date('2002-05-08'); // Your birthday in YYYY-MM-DD format
-const currentDate = new Date();
-const age = currentDate.getFullYear() - birthDate.getFullYear();
-
-const updateAge = () => {
-  document.getElementById('age-span').textContent = age;
-};
-
-document.addEventListener('DOMContentLoaded', updateAge);
